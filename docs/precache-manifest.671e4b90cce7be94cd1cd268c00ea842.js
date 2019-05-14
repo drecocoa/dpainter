7fd4ff192744197376f462275c2d5d7701cdbede
DreDiki <zhanfangkuaile@gmail.com>
2019-05-14 14:11:41 +0800

    build and some change

---
+++ docs/precache-manifest.671e4b90cce7be94cd1cd268c00ea842.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "64fbc413c6de0f083685",
+    "url": "js/chunk-vendors.750fe4cb.js"
+  },
+  {
+    "revision": "38236c816ecafa54efc0",
+    "url": "js/app.adc75dca.js"
+  },
+  {
+    "revision": "e3265da0b912786c641c",
+    "url": "js/about.69260e02.js"
+  },
+  {
+    "revision": "ad5e6cd8a83657bc65f41647fe678538",
+    "url": "index.html"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "38236c816ecafa54efc0",
+    "url": "css/app.3e167b07.css"
+  }
+];
